bfe170a3efde8523c239f67092d09a2c91b78f8a
wojciech170 <wojciech170@gmail.com>
2025-01-24 18:56:33 +0000

    Added .gitignore file and starting to work with new components

---
+++ js/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListItem = () => {
+    return (
+        <div>
+            {/* Your component content goes here */}
+        </div>
+    );
+};
+
+export default ListItem;
